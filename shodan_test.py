90818c6a51df08c3d9108b2d61ce6fc4df98a37c
Joseph Cilenti <jecilenti@gmail.com>
2022-04-13 15:41:59 -0400

    Added additional functionality for search

--- shodan_test.py
+++ shodan_test.py
@@ -16,14 +16,48 @@ def user_prompt():
     user_selection = input("What would you like to do? ")
     if int(user_selection) == 1:
         print("YOU SELECTED IP ADDRESS SEARCH")
-        shodan_ip_search()
+        shodan_host_search()
+    elif int(user_selection) == 2:
+        print("YOU SELECTED KEYWORD SEARCH")
+        shodan_keyword_search()
     prompt_file.close()
 
-def shodan_ip_search():
-    chosen_ip = input("ENTER THE IP ADDRESS YOU'D LIKE TO SEARCH: ")
-    ipinfo = api.host(str(chosen_ip))
-    print(ipinfo)
+def shodan_host_search():
+    # Lookup the host
+    chosen_host = input("ENTER THE IP ADDRESS YOU'D LIKE TO SEARCH: ")
+    host = api.host(str(chosen_host))
 
+    # Print general info
+    print("""
+        IP: {}
+        Organization: {}
+        Operating System: {}
+    """.format(host['ip_str'], host.get('org', 'n/a'), host.get('os', 'n/a')))
+
+    # Print all banners
+    for item in host['data']:
+        print("""
+            Port: {}
+            Banner: {}
+        """.format(item['port'], item['data']))
+
+    #ipinfo = api.host(str(chosen_ip))
+    #print(ipinfo)
+
+def shodan_keyword_search():
+    user_search = input("Enter the service you would like to search for: ")
+    try:
+        # Search Shodan
+        results = api.search(str(user_search))
+
+        # Show the results
+        print('Results found: {}'.format(results['total']))
+        for result in results['matches']:
+                print('IP: {}'.format(result['ip_str']))
+                print(result['data'])
+                print('')
+    except Shodan.APIError as e:
+        print('Error: {}'.format(e))
 
 if __name__ == "__main__":
     read_key()
